955867a0f040b793aaa019b638cde6e854d723bd
kestermbah <156464417+kestermbah@users.noreply.github.com>
2024-07-22 14:21:11 -0400

    Part 2

--- ConsoleApp1/Models/ShoppingCart.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace InventoryManage.Models;
-
-public class ShoppingCart
-{
-    int ID{get; set;}
-    public List<Item>? contents{get; set;}
-}
--- InventoryManagement/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace InventoryManagement;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
